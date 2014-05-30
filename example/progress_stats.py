54f7090a5017802b088c9f80d05b72315a41942a
Geoff van der Meer <gmwils@gmwils-c02gvzkadjwt-2.local>
2014-05-30 15:51:41 +0200

    Example of getting progress statistics

--- example/progress_stats.py
+++ example/progress_stats.py
@@ -22,13 +22,54 @@ OAUTH_CLIENT_NAME = os.environ.get('SKRITTER_OAUTH_CLIENT_NAME')
 OAUTH_CLIENT_SECRET = os.environ.get('SKRITTER_OAUTH_CLIENT_SECRET')
 
 
+def report_monthly_stats(session, start_date, end_date, output_file):
+    progress_stats = skritter.get_progress_stats(session,
+                                                 start_date,
+                                                 end_date,
+                                                 'month')
+    with open(output_file, 'wb') as f:
+        writer = csv.writer(f)
+        header = ['date', 'client', 'days studied', 'time studied']
+        for obj_type in ['word', 'char']:
+            for val in ['defn', 'reading', 'rune', 'tone']:
+                for study_type in ['learned', 'learning', 'remembered', 'studied']:
+                    header += ['%s %s %s' % (obj_type, val, study_type)]
+
+        writer.writerow(header)
+
+        for month in progress_stats:
+            data = []
+            # Basic stats
+            data.append(month.get('date'))
+            data.append(month.get('client'))
+            data.append(month['daysStudied']['all'])
+            data.append(month['timeStudied']['all'])
+
+            # Word stats
+            word = month['word']
+            for obj in [word['defn'], word['rdng'], word['rune'], word['tone']]:
+                add_details(data, obj)
+
+            # Character stats
+            char = month['char']
+            for obj in [char['defn'], char['rdng'], char['rune'], char['tone']]:
+                add_details(data, obj)
+
+            writer.writerow(data)
+
+
+def add_details(data, obj):
+    data.append(obj['learned']['all'])
+    data.append(obj['learning']['all'])
+    data.append(obj['remembered']['all'])
+    data.append(obj['studied']['all'])
+
+
 def main(username, password):
     session = skritter.session(OAUTH_CLIENT_NAME, OAUTH_CLIENT_SECRET)
     session.login(username, password)
 
-    start_date = '2014-05-01'
-    progress_stats = skritter.get_progress_stats(start_date, 'day')
-    print progress_stats
+    report_monthly_stats(session, '2012-05-01', '2014-05-01', 'output.csv')
 
 
 if __name__ == '__main__':
--- skritter/progstats.py
+++ skritter/progstats.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-SKRITTER_VOCABS_URL = 'http://www.skritter.com/api/v0/progstats'
+SKRITTER_PROGSTATS_URL = 'http://www.skritter.com/api/v0/progstats'
 
 
 def get_progress_stats(session, start_date, end_date=None, step='day',
@@ -14,4 +14,20 @@ def get_progress_stats(session, start_date, end_date=None, step='day',
     lang - language to fetch stats for (default: User setting)
     fields - comma separated list of fields to fetch (default: all)
     """
-    return None
+    params = {
+        'start': start_date,
+        'step': step,
+        }
+
+    if end_date is not None:
+        params['end'] = end_date
+
+    if lang is not None:
+        params['lang'] = lang
+
+    if fields is not None:
+        params['fields'] = fields
+
+    response = session.get(SKRITTER_PROGSTATS_URL, params=params)
+
+    return response.get('ProgressStats')
